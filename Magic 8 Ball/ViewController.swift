cfb072b97f3cd4d2ca3a39865d39d67507149e9d
Sarowar H. Mishkat <mishu.sarowar@gmail.com>
2020-01-23 02:56:28 +0600

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Sarowar H. Mishkat on 23/1/20.
+//  Copyright © 2020 Sarowar H. Mishkat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
